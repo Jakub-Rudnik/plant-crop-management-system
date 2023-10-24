d630258741af2eb3fc7fbf8be7eb8d92005e16c1
jakub-rudnik <jakub.rudnik@interia.pl>
2023-10-24 16:32:44 +0200

    Revert "Created route for each site"
    This reverts commit 7817d559eca573235ee99f0867605c6e2ada66e0.

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 $request = $_SERVER['REQUEST_URI'];
-$pagesDir = '/pages/';
 
 switch($request) {
     case '':
@@ -8,15 +7,6 @@ switch($request) {
         echo "Hello!!";
         break;
     case '/uprawy':
-        require __DIR__ . $pagesDir . 'crops.php';
-        break;
-    case '/harmonogram':
-        require __DIR__ . $pagesDir . 'schedule.php';
-        break;
-    case '/pracownicy':
-        require __DIR__ . $pagesDir . 'workers.php';
-        break;
-    default:
-        require __DIR__ . $pagesDir . '404.php';
+        require './pages/crops.php';
 }
 ?>
