c0a335db30ff435567ed2b9a4a279dfabb47baf8
jakub-rudnik <jakub.rudnik@interia.pl>
2023-10-30 22:58:35 +0100

    Changed styles of footer and added closing tag to main wrapper

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,9 @@
 
-<footer>
-    Made with ❤ by
-    <a href="https://github.com/Jakub-Rudnik/plant-crop-management-system">Jakub Rudnik</a>
-    &copy; <?php print date("Y");?>
-</footer>
+    <footer>
+        Made with ❤ by
+        <a href="https://github.com/Jakub-Rudnik/plant-crop-management-system">Jakub Rudnik</a>
+        &copy; <?php print date("Y");?>
+    </footer>
+    </div>
 </body>
 </html>
