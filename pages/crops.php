33206d6b99e88f42d7d1417857e35c893c9d5b2a
jakub-rudnik <jakub.rudnik@interia.pl>
2023-11-20 15:03:57 +0100

    Added cards and site header

--- pages/crops.php
+++ pages/crops.php
@@ -2,7 +2,57 @@
 $title = "Strona Główna | System Ogrodnictwa";
 require_once "./includes/header.php"
 ?>
-<h1>Crops Page</h1>
+<div class="site__header">
+    <h1>Uprawy</h1>
+    <button class="site__header__btn" title="Dodaj uprawę">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+        </svg>
+    </button>
+</div>
+
+<div class="card__wrapper">
+    <div class="card">
+        <h3 class="card__title">
+            Chyzantemy złociste
+        </h3>
+        <div class="card__body">
+            <div class="">
+                <p class="">Wielkość uprawy</p>
+                <p class="">1500</p>
+            </div><div class="">
+                <p class="">Wiglotność podłoża</p>
+                <p class="">76%</p>
+            </div>
+        </div>
+        <div class="card__footer">
+            <a class="card__footer__btn" href="/uprawy">Przejdź</a>
+        </div>
+    </div>
+    <div class="card">
+        <h3 class="card__title">
+            Bratek Szwajciarki Żółty
+        </h3>
+        <div class="card__body">
+            <p class="fontXXL">5</p>
+        </div>
+        <div class="card__footer">
+            <a class="card__footer__btn" href="/uprawy">Przejdź</a>
+        </div>
+    </div>
+    <div class="card">
+        <h3 class="card__title">
+            Bratek Wielokwiatowy Black Knight
+        </h3>
+        <div class="card__body">
+            <p class="fontXXL">5</p>
+        </div>
+        <div class="card__footer">
+            <a class="card__footer__btn" href="/uprawy">Przejdź</a>
+        </div>
+    </div>
+</div>
 <?php
 require_once "./includes/footer.php"
 ?>
